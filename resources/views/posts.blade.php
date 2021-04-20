88c5998f575360359909a32987fd3ea51f6cb359
Ian Travers <mr.itpe@gmail.com>
2021-04-20 14:57:07 +0300

    Use the Filesystem class to read a directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,19 +2,9 @@
 <title>My Blog</title>
 <link rel="stylesheet" href="/app.css">
 <body>
+<?php foreach ($posts as $post) : ?>
 <article>
-    <h1><a href="/posts/first-post">First post</a></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad assumenda aut blanditiis consectetur consequatur
-        culpa cupiditate dolor dolorum eaque, eligendi hic ipsum labore libero maiores modi mollitia nihil omnis, optio
-        possimus provident qui quibusdam, quis quisquam quo quod ratione saepe sed sint soluta totam unde veritatis
-        voluptatem? Aliquam dolorem error facere iusto laborum minus molestiae molestias, necessitatibus provident
-        quisquam quo recusandae repellat repudiandae vitae voluptatem. Atque eveniet nemo, odio odit officia omnis
-        quisquam soluta! Ad assumenda dignissimos minima modi natus officiis praesentium, quaerat rem voluptatem?
-        Commodi, culpa, odio!</p>
+    <?= $post ?>
 </article>
-
-<article>
-    <h1><a href="/posts/second-post">Second post</a></h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, impedit incidunt inventore ipsam itaque natus neque quia reiciendis tenetur unde ut vero. Asperiores corporis esse nihil porro temporibus! Accusantium at, distinctio dolores eius excepturi minima nihil obcaecati perspiciatis placeat soluta ullam, vero vitae voluptatem. Possimus!</p>
-</article>
+<?php endforeach; ?>
 </body>
