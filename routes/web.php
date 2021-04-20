ab85a03701f94db41bf5ec5bce2e3a8599c087b5
Ian Travers <mr.itpe@gmail.com>
2021-04-20 14:36:57 +0300

    Introduce Post class

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,11 +8,5 @@ Route::get('/', function () {
 });
 
 Route::get('/posts/{post}', function ($slug) {
-    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-        return redirect('/');
-    }
-
-    $post = cache()->remember("posts.{$slug}", 3600, fn() => file_get_contents($path));
-
-    return view('post', ['post' => $post]);
+    return view('post', ['post' => Post::find($slug)]);
 })->where('post', '[a-zA-Z\-_]+');
