3cd3f563029d827dde84a3ee633ea467b4846809
Ian Travers <mr.itpe@gmail.com>
2021-04-21 14:51:39 +0300

    Blade Layouts Two Ways: option 1

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,11 @@
-<!DOCTYPE html>
-<title>My Blog</title>
-<link rel="stylesheet" href="/app.css">
-<body>
-<h1>{{ optional($post)->title }}</h1>
-<div>
-    {!! optional($post)->body !!}
-</div>
-<a href="/">Go Back</a>
-</body>
+@extends('layout')
+
+@section('content')
+    <article>
+        <h1>{{ optional($post)->title }}</h1>
+        <div>
+            {!! optional($post)->body !!}
+        </div>
+    </article>
+    <a href="/">Go Back</a>
+@endsection
