36b2a780dc0e66a62aa54fb6386a0f7a2310c6b3
Ian Travers <mr.itpe@gmail.com>
2021-04-21 10:05:40 +0300

    Find a Composer Package for Post Metadata (part 1)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,6 +8,21 @@ use Illuminate\Support\Facades\File;
 
 class Post
 {
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function find(string $slug)
     {
         if (!file_exists($path = resource_path("posts/{$slug}.html") )) {
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
+    $files = File::files(resource_path("posts"));
+    $posts = [];
+
+    foreach ($files as $file) {
+        $document = YamlFrontMatter::parseFile($file);
+        $posts[] = new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
+    }
+
+//    ddd($posts);
+
     return view('posts', [
-        'posts' => Post::all(),
+        'posts' => $posts,
     ]);
 });
 
