b43a01e217a08cdef2104373c19d205a61788ca6
Ian Travers <mr.itpe@gmail.com>
2021-04-21 10:10:59 +0300

    array_map instead foreach (when the foreach loop returns an array)

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     $files = File::files(resource_path("posts"));
-    $posts = [];
 
-    foreach ($files as $file) {
+    $posts = array_map(function ($file) {
         $document = YamlFrontMatter::parseFile($file);
-        $posts[] = new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
-    }
+
+        return new Post($document->title, $document->excerpt, $document->date, $document->body(), $document->slug);
+    }, $files);
 
 //    ddd($posts);
 
