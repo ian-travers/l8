289e1e079025e516e166777e29500688c4f11b60
Ian Travers <mr.itpe@gmail.com>
2021-05-09 10:22:01 +0300

    How to Extract a Dropdown Blade Component (Episode 35)

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,16 @@
+@props(['trigger'])
+
+<div x-data="{ show: false }" @click.away="show = false">
+    {{-- Trigger--}}
+    <div @click="show = !show">
+        {{ $trigger }}
+    </div>
+    {{-- Links--}}
+    <div
+        x-show="show"
+        style="display: none"
+        class="py-2 absolute w-full z-50 bg-gray-100 mt-2 rounded-xl"
+    >
+        {{ $slot }}
+    </div>
+</div>
