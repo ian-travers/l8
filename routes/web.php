4eef6287b96e7c126d33ae93a30669392ddc982e
Ian Travers <mr.itpe@gmail.com>
2021-05-09 09:34:04 +0300

    A Small JavaScript Dropdown Detour (Episode 34)

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts', [
         'posts' => Post::latest()->get(),
+        'categories' => Category::all(),
     ]);
 });
 
@@ -18,11 +19,14 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all(),
     ]);
 });
 
 Route::get('author/{author:username}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts,
+        'categories' => Category::all(),
     ]);
 });
