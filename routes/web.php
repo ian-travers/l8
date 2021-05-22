87df0209f89d0227f42d223947f62f916ccc443f
Ian Travers <mr.itpe@gmail.com>
2021-05-22 15:45:00 +0300

    Author Filtering (episode 41)

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('author/{author:username}', function (User $author) {
-    return view('posts', [
-        'posts' => $author->posts,
-    ]);
-});
