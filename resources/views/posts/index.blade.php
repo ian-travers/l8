ffebb77c16d441df3462ca0d06ccad54b7b1bb46
Ian Travers <mr.itpe@gmail.com>
2021-05-29 15:53:49 +0300

    Laughably Simple Pagination (episode 44)

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,6 +2,12 @@
     @include('posts._header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-posts-grid :posts="$posts"/>
+        @if($posts->count())
+            <x-posts-grid :posts="$posts"/>
+
+            {{ $posts->links() }}
+        @else
+            <p class="text-center">No posts yet. Check back later.</p>
+        @endif
     </main>
 </x-layout>
