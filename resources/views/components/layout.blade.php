175e33f820864732db29cb4fd418380e4829fb11
Ian Travers <mr.itpe@gmail.com>
2021-06-10 14:09:05 +0300

    Login and Logout (episode 49)

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,8 +16,23 @@
             </a>
         </div>
 
-        <div class="mt-8 md:mt-0">
-            <a href="/" class="text-xs font-bold uppercase">Home Page</a>
+        <div class="mt-8 md:mt-0 flex items-center">
+            @auth
+                <span class="text-xs uppercase font-bold">Welcome, {{ auth()->user()->name }}</span>
+
+                <form action="/logout" method="post">
+                    @csrf
+                    <button
+                        type="submit"
+                        class="text-xs font-semibold text-blue-500 ml-6"
+                    >
+                        Logout
+                    </button>
+                </form>
+            @else
+                <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                <a href="/login" class="ml-3 text-xs font-bold uppercase">Login</a>
+            @endauth
 
             <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                 Subscribe for Updates
