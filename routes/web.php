457a932355ce948e45a34437ab1df93215b0b805
Ian Travers <mr.itpe@gmail.com>
2021-07-14 14:41:36 +0300

    Make the Newsletter Form Work (episode 59)

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,11 @@ Route::group([
     Route::post('logout', [SessionsController::class, 'destroy']);
 });
 
-Route::get('ping', function () {
+Route::post('newsletter', function () {
+    request()->validate([
+        'email' => 'required|email',
+    ]);
+
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
     $mailchimp->setConfig([
@@ -34,10 +38,17 @@ Route::get('ping', function () {
         'server' => 'us6',
     ]);
 
-    $response = $mailchimp->lists->addListMember('e7d8479f61', [
-        'email_address' => 'mr.itpe@gmail.com',
-        'status' => 'subscribed',
-    ]);
+    try {
+        $mailchimp->lists->addListMember('e7d8479f61', [
+            'email_address' => request('email'),
+            'status' => 'subscribed',
+        ]);
+    } catch (Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
 
-    ddd($response);
+
+    return redirect('/')->with('success', 'You are now signed up for our newsletter! ');
 });
