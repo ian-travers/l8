cadc7fc91fdce4ba8f3b381bef4ab8b034a20662
Ian Travers <mr.itpe@gmail.com>
2021-07-16 11:16:22 +0300

    Extract a Newsletter Service (episode 60)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -10,6 +11,8 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::group([
     'middleware' => 'guest',
@@ -24,31 +27,4 @@ Route::group([
     'middleware' => 'auth',
 ], function () {
     Route::post('logout', [SessionsController::class, 'destroy']);
-});
-
-Route::post('newsletter', function () {
-    request()->validate([
-        'email' => 'required|email',
-    ]);
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us6',
-    ]);
-
-    try {
-        $mailchimp->lists->addListMember('e7d8479f61', [
-            'email_address' => request('email'),
-            'status' => 'subscribed',
-        ]);
-    } catch (Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.'
-        ]);
-    }
-
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter! ');
 });
